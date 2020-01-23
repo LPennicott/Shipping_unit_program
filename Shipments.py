88ec276903050d20d3aa1891f780630c95a805ce
LPennicott <54121651+LPennicott@users.noreply.github.com>
2020-01-23 11:01:55 -0500

    First Post

---
+++ Shipments.py
@@ -0,0 +1,146 @@
+import datetime, sqlite3
+conn = sqlite3.connect('Shipping_Units (2).db')
+curs = conn.cursor()
+
+class Shipment:
+    """docstring for Shipment"""
+    def __init__(self, 
+                client, 
+                width, 
+                length, 
+                height, 
+                gross_weight, 
+                purpose, 
+                on_hand_number = None, 
+                create_date = datetime.datetime.today().strftime('%Y-%m-%d'), 
+                release_date = None,
+                mawb = None,
+                hawb = None):
+        self.client = client
+        self.width = width
+        self.length = length
+        self.height = height
+        self.gross_weight = gross_weight
+        self.purpose = purpose
+        self.on_hand_number = on_hand_number
+        self.create_date = create_date
+        self.release_date = release_date
+        self.mawb = mawb
+        self.hawb = hawb
+
+    def __str__(self):
+        return (
+            f'''
+            This shipment belongs to client: {self.client}
+            Dimensions: {self.width} x {self.length} x {self.height} 
+            Gross Weight: {self.gross_weight} lbs
+            On Hand Number: {self.on_hand_number}
+            Purpose: {self.purpose}
+            Created on: {self.create_date}
+            Released on: {self.release_date}
+            '''
+            ).lstrip()
+
+    def volume_weight(self):
+        return str(round((self.width * self.length * self.height)/366, 1)) + " kg/vol"
+
+    def lbs_to_kg(self):
+        return round(self.gross_weight/2.204)
+
+    def add_shipment(self):
+        curs.execute('INSERT INTO shipments VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (
+            self.on_hand_number, self.client, self.width, self.length, 
+            self.height, self.gross_weight, self.purpose,
+            self.create_date, self.release_date, self.mawb, self.hawb))
+        return conn.commit()
+
+    def delete_shipment(self):
+        curs.execute('DELETE FROM shipments WHERE on_hand_number = ?',(
+            self.on_hand_number,))
+        return conn.commit()
+
+    def update_shipment(self, client, width, length, height, gross_weight, purpose):
+        curs.execute(
+            '''
+            UPDATE shipments
+            SET client = ?,
+            width = ?,
+            length = ?,
+            height = ?,
+            gross_weight = ?,
+            purpose = ?
+            WHERE on_hand_number = ?
+            ''', (client, width, length, height, gross_weight, purpose,
+            self.on_hand_number))
+        return conn.commit()
+
+    def release_shipment(self, MAWB, HAWB):
+        curs.execute(
+            '''
+            UPDATE shipments
+            SET release_date = ?,
+            MAWB = ?,
+            HAWB = ?
+            WHERE on_hand_number = ?
+            ''', (datetime.datetime.today().strftime('%Y-%m-%d'), MAWB, HAWB, self.on_hand_number))
+        return conn.commit()
+
+    def add_to_consolidation(self, MAWB, HAWB):
+        curs.execute(
+            '''
+            UPDATE shipments
+            SET MAWB = ?,
+            HAWB = ?,
+            WHERE on_hand_number = ?
+            ''', (MAWB, HAWB, self.on_hand_number))
+        return conn.commit()
+
+    def update_MAWB(self, MAWB, HAWB, unit_count, gross_weight, volume_weight, date):
+        curs.execute(
+            '''
+            INSERT INTO consols VALUES ?, ?, ?, ?, ?, ?
+            ''', (MAWB, HAWB, unit_count, gross_weight, volume_weight, date))
+        return conn.commit()
+
+    @staticmethod
+    def fetch_all_records(quantity = 50):
+        curs.execute('SELECT * FROM shipments ORDER BY create_date DESC LIMIT ?', 
+            (quantity,))
+        return curs.fetchall()
+
+    @staticmethod
+    def shipment_count():
+        curs.execute('SELECT * FROM shipments')
+        return len(curs.fetchall())
+
+    @staticmethod
+    def find_one_record(on_hand_number):
+        curs.execute('SELECT * FROM shipments WHERE on_hand_number = ?',
+        (on_hand_number,))
+        param = curs.fetchall()[0]
+        return Shipment(*param)
+
+    @staticmethod #decorator for export to csv?
+    def fetch_all_records_in_date(first_date, second_date):
+        curs.execute('SELECT * FROM shipments WHERE create_date BETWEEN ? AND ?',
+            (first_date, second_date))
+        return curs.fetchall()
+
+    @staticmethod
+    def view_all_units_inhouse():
+        curs.execute('SELECT * FROM shipments WHERE release_date IS NULL')
+        return curs.fetchall()
+
+    @staticmethod
+    def view_mawbs():
+        curs.execute('SELECT * FROM consols')
+        return curs.fetchall()
+
+if __name__ == '__main__':
+    test = Shipment('TSB', 48, 40, 60, 460, 'Export', 10003)
+    test2 = Shipment('Gems', 48, 40, 60, 400, 'Export', 10004)
+    print(test)
+    print(test.volume_weight())
+    test.add_shipment()
+    test2.add_shipment()
+    conn.close()
---
+++ main_menu.py
@@ -0,0 +1,53 @@
+import PySimpleGUI as sg
+import shipping_unit_gui 
+
+#Main window to stay active for duration of the program. Other options occur in Multi-window.
+#Add ability to add comments to shipping units
+#another database table for MAWB consols
+
+layout = [
+	[sg.Text('Shipping Unit Program!' , size = (30, 1), font = ('Comic', 25))],
+	[sg.Text('Please select an option below or search for a shipment', 
+		size = (30, 2), font = ('Comic', 15))],
+	[sg.Text('Search with on-hand number', size = (25, 1)), 
+	sg.InputText(key = 'search on-hand')],
+    [sg.Button('Add Shipment', key = 'save_new'),
+    sg.Button('View Shipments', key = 'view'),
+    sg.Button('Export', key = 'export'),
+    sg.Button('Search', key = 'search'),
+    sg.Button('Consolidation', key = 'consol'),
+    sg.Button('Report', key = 'report'),
+    ],
+    [sg.Cancel()]
+]
+
+sg.change_look_and_feel('DarkBlue')
+
+window = (sg.Window('Main Menu', layout, resizable = True))
+event, values = window.read()
+
+window.close()
+
+if event == 'save_new':
+    shipping_unit_gui.save_new_shipment()
+elif event == 'view':
+	shipping_unit_gui.view_shipments()
+elif event == 'search':
+    try:
+	    shipping_unit_gui.lookup_a_shipment(values['search on-hand'])
+    except:
+        layout2 = [[sg.Text('Shipping Unit not found! Click below to view shipments.')],
+        [sg.Submit()]]
+        sg.change_look_and_feel('DarkBlue')
+
+        window2 = (sg.Window('Shipment not found!', layout2))
+        event, values = window2.read()
+        window2.close()
+        shipping_unit_gui.view_shipments()
+elif event == 'export':
+	shipping_unit_gui.export_to_csv()
+elif event == 'report':
+    shipping_unit_gui.report()
+elif event == 'consol':
+    shipping_unit_gui.consolidation()
+	
